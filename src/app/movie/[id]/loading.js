5045793de7c3c39f89bfbfd769c1f9146c642010
User <huseynovali612@gmail.com>
2023-11-26 21:59:37 +0400

    create not found  page

---
+++ src/app/movie/[id]/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='text-5xl text-red-600'>loading...</div>
+  )
+}
+
+export default Loading
--- src/services/movie.js
+++ src/services/movie.js
@@ -1,19 +1,22 @@
 export async function getStaticProps(title) {
-  const url = `https://api.themoviedb.org/3/movie/${title}?language=en-US&page=1`
-  
-  //popular?api_key=${process.env.api_key}&language=en-US&page=1`;
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: process.env.api_key
-    }
-  };
-  const response = await fetch(url,options);
-  const fetchdata = await response.json();
+  try {
+    const url = `https://api.themoviedb.org/3/movie/${title}?language=en-US&page=1`;
 
-  return {
-    fetchdata,
-  };
-} 
-  
+    //popular?api_key=${process.env.api_key}&language=en-US&page=1`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.api_key,
+      },
+    };
+    const response = await fetch(url, options);
+    const fetchdata = await response.json();
+
+    return {
+      fetchdata,
+    };
+  } catch (error) {
+    return error
+  }
+}
